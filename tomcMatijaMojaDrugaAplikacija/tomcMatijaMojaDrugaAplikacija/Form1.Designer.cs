c046403eed9a1d5303cc2355bcf9d7bc29534f1a
matijatomc <55134628+matijatomc@users.noreply.github.com>
2019-09-24 12:12:57 +0200

    Add files via upload

---
+++ tomcMatijaMojaDrugaAplikacija/tomcMatijaMojaDrugaAplikacija/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace tomcMatijaMojaDrugaAplikacija
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.txtPrikaz = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(73, 227);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(99, 71);
+            this.btnMinus.TabIndex = 0;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.BtnMinus_Click);
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(574, 227);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(99, 71);
+            this.btnPlus.TabIndex = 1;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // txtPrikaz
+            // 
+            this.txtPrikaz.Location = new System.Drawing.Point(339, 110);
+            this.txtPrikaz.Name = "txtPrikaz";
+            this.txtPrikaz.Size = new System.Drawing.Size(112, 20);
+            this.txtPrikaz.TabIndex = 2;
+            this.txtPrikaz.Text = "0";
+            this.txtPrikaz.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtPrikaz);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.btnMinus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.TextBox txtPrikaz;
+    }
+}
+
---
+++ tomcMatijaMojaDrugaAplikacija/tomcMatijaMojaDrugaAplikacija/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tomcMatijaMojaDrugaAplikacija
+{
+    public partial class Form1 : Form
+    {
+        int br = 0;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            br++;
+            txtPrikaz.Text = br.ToString();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnMinus_Click(object sender, EventArgs e)
+        {
+            br--;
+            txtPrikaz.Text = br.ToString();
+        }
+    }
+}
